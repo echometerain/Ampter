9e52a4046f472b233f86edf0205445dfcae6908a
Zaniff <ieatcrayonseveryday@gmail.com>
2023-03-04 01:08:15 -0500

    forgot we were doing customtkinter

--- frontend.py
+++ frontend.py
@@ -1,4 +1,4 @@
-import ampter as amp
+#import ampter as amp
 import numpy as np
 import scipy
 import pedalboard as pb
@@ -9,12 +9,28 @@ song_path = None  # path of the song
 brush_path = None  # path of the VST plugin
 Y_PADDING = 500  # how thick the brush is (y direction)
 
-window = tk.Tk()
-window.geometry("2500x2000")
+#comments are to help me remember whats what lol
 
-uploadButton = tk.Button(text = "Upload File",
-                width = 10,
-                height = 10)
-uploadButton.pack()
 
-window.mainloop()
+ctk.set_appearance_mode("System")       
+ctk.set_default_color_theme("blue")   
+class App(ctk.CTk):
+# Layout of the GUI will be written in the init itself
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+# Sets the title of window 
+        self.title("Ampter - Audio Mastering Painter")   
+# Dimensions of the window will be 200x200
+        self.geometry("2500x2000")   #it had to be at least 2000 pixels tall right?
+
+        self.nameLabel = ctk.CTkLabel(self,
+                                text="Upload A File")
+        self.nameLabel.grid(row=0, column=0,
+                            padx=20, pady=10,
+                            sticky="ew")
+        
+
+if __name__ == "__main__":
+    app = App()
+    # Runs the app
+    app.mainloop()  
