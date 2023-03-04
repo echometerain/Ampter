71d0352c07f776354e9104eeccebab38b2f4404c
Zaniff <ieatcrayonseveryday@gmail.com>
2023-03-04 01:33:57 -0500

    file name record

--- frontend.py
+++ frontend.py
@@ -23,14 +23,56 @@ class App(ctk.CTk):
 # Dimensions of the window will be 200x200
         self.geometry("2500x2000")   #it had to be at least 2000 pixels tall right?
 
+        # Name Label
         self.nameLabel = ctk.CTkLabel(self,
-                                text="Upload A File")
+                                text="File Name:")
         self.nameLabel.grid(row=0, column=0,
-                            padx=20, pady=10,
+                            padx=20, pady=20,
                             sticky="ew")
+ 
+        # Name Entry Field
+        self.fileEntry = ctk.CTkEntry(self,
+                          placeholder_text="enter here")
+        self.fileEntry.grid(row=1, column=0,
+                            columnspan=3, padx=20,
+                            pady=20, sticky="ew")
         
-
+        # Generate Button
+        self.generateFileButton = ctk.CTkButton(self,
+                                         text="Upload File",
+                                         command=self.generateResults)
+        self.generateFileButton.grid(row=0, column=1,
+                                        columnspan=2, padx=20,
+                                        pady=20, sticky="ew")
+ 
+        # Text Box
+        self.displayBox = ctk.CTkTextbox(self,
+                                         width=200,
+                                         height=100)
+        self.displayBox.grid(row=2, column=0,
+                             columnspan=4, padx=20,
+                             pady=20, sticky="nsew")
+ 
+ 
+    # This function is used to insert the
+    # details entered by users into the textbox
+    def generateResults(self):
+        self.displayBox.delete("0.0", "200.0")
+        text = self.createText()
+        self.displayBox.insert("0.0", text)
+ 
+    # This function is used to get the selected
+    # options and text from the available entry
+    # fields and boxes and then generates
+    # a prompt using them
+    def createText(self):
+        checkboxValue = ""
+        # Constructing the text variable
+        text = f"{self.fileEntry.get()}"
+ 
+        return text
+ 
 if __name__ == "__main__":
     app = App()
-    # Runs the app
-    app.mainloop()  
+    # Used to run the application
+    app.mainloop()
