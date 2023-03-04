d4a9b4da7a5c080bb60d55bbd34cbb504f68db82
Zaniff <ieatcrayonseveryday@gmail.com>
2023-03-04 00:50:56 -0500

    temporary frontpage

--- frontend.py
+++ frontend.py
@@ -8,3 +8,13 @@ import customtkinter as ctk
 song_path = None  # path of the song
 brush_path = None  # path of the VST plugin
 Y_PADDING = 500  # how thick the brush is (y direction)
+
+window = tk.Tk()
+window.geometry("2500x2000")
+
+uploadButton = tk.Button(text = "Upload File",
+                width = 10,
+                height = 10)
+uploadButton.pack()
+
+window.mainloop()
