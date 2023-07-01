998779e49f2e1c2190f3c6787d2c93267eb2a19f
echometerain <echometer@disroot.org>
2023-07-01 09:05:09 -0400

    restart

--- ampter.py
+++ ampter.py
@@ -5,17 +5,21 @@ import backend as be
 import frontend as fe
 from multiprocessing import Process
 import time
+from PIL import Image
 
-be.load_song("sine.mp3")
+be.load_song("sewersample.mp3")
+be.get_spectrogram()
+img = Image.open(be.spec)
+img.show()
 
 be.load_brush("/usr/lib/vst3/Auburn Sounds Panagement 2.vst3")
 # be.isolate()
 # be.get_spectrogram()
 # be.show_spec()
 
-player = Process(target=be.play)
-player.start()
-time.sleep(5)
-player.terminate()
+# player = Process(target=be.play)
+# player.start()
+# time.sleep(5)
+# player.terminate()
 # app = fe.App()
 # app.mainloop()
